1e4d4fe2ef5734df5a76d86adb1dc8b198670eca
viniolimpio3 <viniolimpio3@gmail.com>
2021-09-04 12:20:59 -0300

    Desafio de sequência números EC2

---
+++ solucoesConsole/solucoesConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace solucoesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Métodos
+            Console.WriteLine("Hello World!");
+            Program o = new Program();
+
+
+        }
+
+        public void exibeTextoVermelho(String text){
+            //return 
+        }
+
+        
+    }
+}
